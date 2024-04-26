b0e6e549f341b9ec93918f5875b04b035c1e2678
Gabriel Bustos <gabrielbustos0905@gmail.com>
2024-04-26 15:37:56 -0300

    first commit

---
+++ src/services/spacex.ts
@@ -0,0 +1,31 @@
+import { type Doc, type APIScpaceXResponse } from '../types/api'
+
+export const getLatestLaunches = async () => {
+  const res = await fetch('https://api.spacexdata.com/v5/launches/query', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      query: {},
+      options: {
+        sort: {
+          date_unix: 'asc'
+        },
+        limit: 24
+      }
+    })
+  })
+
+  const { docs: launches } = await res.json() as APIScpaceXResponse
+
+  return launches
+}
+
+export const getLaunchById = async ({ id }: { id: string }) => {
+  const res = await fetch(`https://api.spacexdata.com/v5/launches/${id}`)
+
+  const launch = await res.json() as Doc
+
+  return launch
+}
